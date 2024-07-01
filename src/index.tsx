f472c3c5a12e0e552106eca4690fb97943078f8e
James Gordo <gordo.jt@sprobe.com>
2024-07-01 13:14:51 +0800

    Added Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -9,29 +9,35 @@ type Country = {
   capital: string[];
   currencies: { [currencyCode: string]: Currency };
   region: string;
-}
+};
 
 export default function useCountries() {
   const items: Country[] = useMemo(
     () =>
       countries
         .sort((a, b) => a.name.common.localeCompare(b.name.common))
-        .map((country) => ({
-          name: country.name.common,
-          alpha2: country.cca2,
-          alpha3: country.cca3,
-          flag: country.flag,
-          capital: country.capital,
-          currencies: country.currencies,
-          region: country.region,
-        }) as Country),
+        .map(
+          (country) =>
+            ({
+              name: country.name.common,
+              alpha2: country.cca2,
+              alpha3: country.cca3,
+              flag: country.flag,
+              capital: country.capital,
+              currencies: country.currencies,
+              region: country.region,
+            }) as Country
+        ),
     countries
   );
 
   const getByCode = (code: string) =>
-    items.filter((country) => [country.alpha2, country.alpha3].includes(code)) as Country[];
+    items.filter((country) =>
+      [country.alpha2, country.alpha3].includes(code)
+    ) as Country[];
 
-  const getByName = (name: string) => items.filter((country) => name === country.name) as Country[];
+  const getByName = (name: string) =>
+    items.filter((country) => name === country.name) as Country[];
 
   return { countries: items, getByCode, getByName };
 }
